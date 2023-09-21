38fe1bcba16c4d124867a2e3dcc388b0798d16ea
Gilbert Omosa <gilbert.omosa@student.moringaschool.com>
2023-09-21 17:39:05 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName = "bob";
+
+function upperCaseCustomerName() {
+
+    return customerName = customerName.toUpperCase();
+
+}
+
+function setBestCustomer() {
+
+  bestCustomer = 'not bob';
+
+}
+
+function overwriteBestCustomer() {
+
+  bestCustomer = 'maybe bob';
+
+}
+
+const leastFavoriteCustomer = 'Gilbert';
+
+function changeLeastFavoriteCustomer() {
+
+  leastFavoriteCustomer = 'Omosa';
+
+}
